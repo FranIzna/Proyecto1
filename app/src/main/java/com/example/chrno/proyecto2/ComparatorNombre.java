5fe80a6d95009da2162282e43e2e0c6540ed603c
FranIzna <Fran_Izna@hotmail.com>
2015-10-19 12:27:29 +0200

    ultima actualizacion

--- app/src/main/java/com/example/chrno/proyecto2/ComparatorNombre.java
+++ app/src/main/java/com/example/chrno/proyecto2/ComparatorNombre.java
@@ -1,13 +1,12 @@
 package com.example.chrno.proyecto2;
 
-import java.util.*;
-
 /**
  * Created by 2dam on 19/10/2015.
  */
 public class ComparatorNombre implements java.util.Comparator<Contacto> {
     @Override
     public int compare(Contacto c, Contacto c1) {
-        return c1.getNombre().compareTo(c.getNombre());
+        return c1.getNombre().compareToIgnoreCase(c.getNombre());
+
     }
 }
